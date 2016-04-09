fda2076dfe073d8f993fd418767370e493c8b540
Sulejman Sarajlija <sulejmansarajlija@gmail.com>
2016-04-09 13:37:12 +0200

    initial commit

---
+++ academy-project/api/models/User.js
@@ -0,0 +1,52 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+      firstName : {
+          type: 'string', 
+          required: true
+      },
+      lastName : {
+          type: 'string', 
+          required: true
+      },
+      birthDate: {
+          type: 'date', 
+          required: true
+      },
+      username: {
+            type: 'string',
+            required: true,
+            alphanumeric: true,
+            maxLength: 20,
+            unique: true   
+      },
+      country:{
+          type: 'string', 
+          enum: ['Bosnia and Herzegovina', 'Croatia', 'Serbia', 'Slovenia', 'Montenegro', 'USA']
+      },
+      image: {
+        type: 'string', 
+        defaultsTo: 'http://www.freebanking.org/avatars/schuler.png'  
+      },
+      email: {
+        type: 'string',
+        email: true, 
+        required: true, 
+        unique: true  
+      },
+      gender: {
+            type: 'string',
+            enum: ['Male', 'Female', 'Other'],
+            required: true
+      }
+  },
+  
+};
+
